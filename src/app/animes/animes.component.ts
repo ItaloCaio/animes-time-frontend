f87489fe44a1f1952efcc75d5d6045bb5c56a880
Carvalho <italo.caio.carvalho@accenture.com>
2019-11-12 15:05:12 -0300

    Criando o esqueleto da primeira tela

---
+++ src/app/animes/animes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'animes-selector',
+  templateUrl: './animes.component.html',
+  styleUrls: ['./animes.component.css']
+})
+export class AnimesComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { TopComponent } from './top/top.component';
+import { AnimesComponent } from './animes/animes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MenuComponent, TopComponent, AnimesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, MenuComponent, TopComponent, AnimesComponent]
 })
 export class AppModule { }
